003f376d298e72d187bd019ff3e7930d75921701
Eduardo Gomes <edugomes@fb.com>
2017-09-21 19:53:45 -0300

    Other SendAPI features

--- app/helpers/facebookGraphAPI.js
+++ app/helpers/facebookGraphAPI.js
@@ -22,70 +22,165 @@ module.exports = {
     messageData = {
       text:text
     };
-    request({
-      url: 'https://graph.facebook.com/v2.6/me/messages',
-      qs: {access_token:config.page_access_token},
-      method: 'POST',
-      json: {
-        recipient: {id: sender},
-        message: messageData
-      }
-    }, function(error, response, body) {
-      if (error) {
-        console.log('Error sending messages: ', error);
-      } else if (response.body.error) {
-        console.log('Error: ', response.body.error);
-      }
-    });
+    postMessage(sender, messageData);
   },
 
-  sendGenericMessage: function sendGenericMessage(sender) {
+  sendAudioMessage: function sendAudioMessage(sender, audioUrl) {
+    messageData = {
+      attachment:{
+        type: "audio",
+        payload: {
+          url: audioUrl
+        }
+      }
+    };
+    postMessage(sender, messageData);
+  },
+
+  sendFileMessage: function sendFile(sender, fileUrl) {
+    messageData = {
+      attachment:{
+        type: "file",
+        payload: {
+          url: fileUrl
+        }
+      }
+    };
+    postMessage(sender, messageData);
+  },
+
+  sendVideoMessage: function sendVideo(sender, videoUrl) {
+    messageData = {
+      attachment:{
+        type: "video",
+        payload: {
+          url: videoUrl
+        }
+      }
+    };
+    postMessage(sender, messageData);
+  },
+
+  sendImageMessage: function sendImage(sender, imageUrl) {
+    messageData = {
+      attachment:{
+        type: "image",
+        payload: {
+          url: imageUrl
+        }
+      }
+    };
+    postMessage(sender, messageData);
+  },
+
+  createWebUrlTemplate: function createWebUrlTemplate (url, title) {
+      return {
+        type: "web_url",
+        url: url,
+        title: title
+      };
+  }, 
+
+  createPostbackTemplate: function createPostbackTemplate (title, payload) {
+    return {
+      type: "postback",
+      title: title,
+      payload: payload
+    };
+  }, 
+
+  sendButtonsTemplate: function sendButtonsTemplate(sender, text, buttons) {
+    messageData = {
+      attachment:{
+        type: "template",
+        payload: {
+          template_type: "button",
+          text: text,
+          buttons: buttons
+        }
+      }
+    };
+    postMessage(sender, messageData);
+  },
+
+  createCardTemplate: function createCardTemplate (title, subtitle, imageUrl, buttons) {
+    return {
+      "title": title,
+      "subtitle": subtitle,
+      "image_url": imageUrl,
+      "buttons": buttons
+    };
+  }, 
+
+  createTextQuickReply: function createTextQuickReply (title, payload, imageUrl) {
+    let template = {
+      "content_type": "text",
+      "title": title,
+      "payload": payload
+    };
+    if (imageUrl)
+      template.image_url = imageUrl;
+
+    return template;
+  }, 
+
+  createLocationQuickReply: function createLocationQuickReply (title, subtitle, imageUrl, buttons) {
+    return {
+      "content_type": "location",
+    };
+  }, 
+  
+  sendGenericTemplate: function sendGenericTemplate(sender, cards) {
     messageData = {
       "attachment": {
         "type": "template",
         "payload": {
           "template_type": "generic",
-          "elements": [{
-            "title": "First card",
-            "subtitle": "Element #1 of an hscroll",
-            "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-            "buttons": [{
-              "type": "web_url",
-              "url": "https://www.messenger.com/",
-              "title": "Web url"
-            }, {
-              "type": "postback",
-              "title": "Postback",
-              "payload": "Payload for first element in a generic bubble",
-            }],
-          },{
-            "title": "Second card",
-            "subtitle": "Element #2 of an hscroll",
-            "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-            "buttons": [{
-              "type": "postback",
-              "title": "Postback",
-              "payload": "Payload for second element in a generic bubble",
-            }],
-          }]
+          "elements": cards
         }
       }
     };
+    postMessage(sender, messageData);
+  },
+  createSenderActionMarkSeen: () => { return "mark_seen"; },
+  createSenderActionTypingOn: () => { return "typing_on"; },
+  createSenderActionTypingOff: () => { return "typing_off"; },
+  
+  sendSenderAction: function sendSenderAction(sender, action){
     request({
       url: 'https://graph.facebook.com/v2.6/me/messages',
-      qs: {access_token:config.page_access_token},
+      qs: { access_token: config.page_access_token },
       method: 'POST',
       json: {
-        recipient: {id:sender},
-        message: messageData,
-      }
+      recipient: { id: sender },
+      sender_action: action
+    }
     }, function(error, response, body) {
-      if (error) {
-        console.log('Error sending message: ', error);
-      } else if (response.body.error) {
+        if(error) {
+        console.log('Error sending messages: ', error);
+    }
+    else if(response.body.error) {
         console.log('Error: ', response.body.error);
-      }
+    }
     });
   }
-
 };
+function postMessage(sender, messageData) {
+    request({
+      url: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: { access_token: config.page_access_token },
+      method: 'POST',
+      json: {
+      recipient: { id: sender },
+      message: messageData
+    }
+    }, function(error, response, body) {
+        if(error) {
+        console.log('Error sending messages: ', error);
+    }
+    else if(response.body.error) {
+        console.log('Error: ', response.body.error);
+    }
+    });
+}
+
