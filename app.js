b7e61f72ae0ef70e35ca9c3d18fcf802364bf944
Eduardo Gomes <edugomes@fb.com>
2017-10-12 19:13:05 -0300

    Documentation

--- app.js
+++ app.js
@@ -7,17 +7,4 @@ require('./config/express')(app, config);
 
 app.listen(config.port || 9000, function () {
     console.log('Express server started and listening on ' + this.address().port + ".");
-})
-
-/* The following snippet can be used to trigger an action accordingly to a CRON pattern
-* e.g.: triggering a work chat message to an user.
-*/
-// let job = new CronJob('00 * * * * 1-5', function() {
-//     /*
-//    * Runs every hour (minute 00), form Monday through Friday
-//    */
-
-//     // Execute code here
-
-// }, null, true, 'America/Los_Angeles');
-// job.start();
+});
